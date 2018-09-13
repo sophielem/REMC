79b535e3961522cefc87ea4284ec826d71d56f2b
sophielem <sophie.lematre@yahoo.com>
2018-09-13 10:36:27 +0200

    comment code

--- src/End_moves.py
+++ src/End_moves.py
@@ -3,10 +3,13 @@ import random
 import Movement
 
 class End_moves(Movement.Movement):
-  """ text
+  """ The residue is pivoted relative to its connected neighbour to a free position adjacent to that neighbour.
   """
 
   def mutation(self, structure_grid):
+      """ Search free neighbour for the residu adjacent. If several positions are free, on
+          is chosen randomly and the residu is pivoted to this position.
+      """
     if self.index == 0:
       freedom_neighbour_list = self.residu.next_res.search_free_neighbour(structure_grid)
     else:
@@ -19,5 +22,9 @@ class End_moves(Movement.Movement):
 
 
   def __init__(self, res, i):
+      """ Initialize the object End_moves
+          :param res: Residu object
+          :param   i: index of the Residu object
+      """
       Movement.Movement.__init__(self, i)
       self.residu = res
--- src/Movement.py
+++ src/Movement.py
@@ -2,16 +2,21 @@
 import checkingArgument as cA
 import copy
 class Movement():
+    """ Mother class of all movements.
+    """
 
     def countBonds(self, structure_grid, residues):
+        """ Counts the number of bonds. For each bond, an energy of -1 is summed.
+            Return the total energy.
+        """
         bonds = 0
-        # Liste tous les liens deja compte pour ne pas avoir de doublons
+        # List all bounds already counted to avoid duplicate
         counted_bond = []
         for res in range(cA.LEN_SEQ):
-            # Si polaire, pas de liaison possible donc suivant
+            # If residu is polar, non bound possible so next
             if cA.SEQUENCE[res] == 'P':
                 continue
-            # Contient tous les voisins
+            # Contains all neighbours, free or not
             index = []
             index.append(structure_grid[residues[res].line][residues[res].column + 1])
             index.append(structure_grid[residues[res].line][residues[res].column - 1])
@@ -19,7 +24,7 @@ class Movement():
             index.append(structure_grid[residues[res].line - 1][residues[res].column ])
 
             for idx in index:
-                # res+1 et res-1 vérifier que le résidu n'est pas un voisin sur la séquence
+                # check neighbour is not adjacent in the sequence, it's hydrophob and not already counted
                 if (idx != res+1) and (idx != res-1) and (residues[idx].hp == 'H') and (not sorted([idx, res]) in counted_bond):
                     counted_bond.append(sorted([idx, res]))
                     bonds -= 1
@@ -28,14 +33,17 @@ class Movement():
 
 
     def changeOneResidu(self, structure_grid, residues, random_neighbour):
-        # copie les résidus et la grille et assigne la nouvelle conformation
+        """ Change the conformation of one residu.
+            Return the new conformation and residues obejct modified.
+        """
+        # copy residues and the lattice. Assign the new conformation
         structure_grid_new = copy.deepcopy(structure_grid)
-        # l ancienne position du residu devient vide
+        # the old position of residu becomes empty
         structure_grid_new[residues[self.index].line][residues[self.index].column] = -1
         structure_grid_new[random_neighbour['line']][random_neighbour['column']] = self.index
 
         residues_new = copy.deepcopy(residues)
-        # attribution des nouvelles coordonnées
+        # Assignement of new coordinates
         residues_new[self.index].line = random_neighbour['line']
         residues_new[self.index].column = random_neighbour['column']
 
@@ -43,18 +51,21 @@ class Movement():
 
 
     def changeTwoResidues(self, structure_grid, residues, random_neighbour):
+        """ Change the conformation of two residues.
+            Return the new conformation and reidues objet modified.
+        """
         cote = random_neighbour[2]
-        # copie les résidus et la grille et assigne la nouvelle conformation
+        # copy residues and the lattice, assign the new conformation
         structure_grid_new = copy.deepcopy(structure_grid)
-        # l ancienne position du residu devient vide
+        # the old positions of residues become empty
         structure_grid_new[residues[self.index].line][residues[self.index].column] = -1
         structure_grid_new[random_neighbour[0]['line']][random_neighbour[0]['column']] = self.index
         # Second residu
         structure_grid_new[residues[self.index + cote].line][residues[self.index + cote].column] = -1
-        structure_grid_new[random_neighbour[1]['line']][random_neighbour[1]['column']] = self.index + cote       
+        structure_grid_new[random_neighbour[1]['line']][random_neighbour[1]['column']] = self.index + cote
 
         residues_new = copy.deepcopy(residues)
-        # attribution des nouvelles coordonnées
+        # Assignementof new coordinates
         residues_new[self.index].line = random_neighbour[0]['line']
         residues_new[self.index].column = random_neighbour[0]['column']
         # Second residu
@@ -65,4 +76,7 @@ class Movement():
 
 
     def __init__(self, i):
+      """ Initialize the object Movement
+          :param   i: index of the Residu object
+      """
         self.index = i
